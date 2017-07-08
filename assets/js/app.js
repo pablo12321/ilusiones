3ba07ae208f08efd75b4f5fa488ac8f3f64089ee
Pablo <pabloandroetto@outlook.com>
2017-07-08 20:00:09 -0300

    Avance en clientes

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,52 +3,74 @@ var baseapi = "http://localhost/ilusiones";
 var pagesF = "pages/";
 var menu = [
   {
+    enmenu:true,
     nombre:'Resumen',
     ruta:'/',
     url:'resumen.html',
     ico:'dashboard',
   },
   {
+    enmenu:true,
     nombre:'Productos',
     ruta:'/productos',
     url:'productos.html',
     ico:'trophy',
   },
   {
+    enmenu:true,
     nombre:'Proveedores',
     ruta:'/proveedores',
     url:'proveedores.html',
     ico:'id-card',
   },
   {
+    enmenu:true,
     nombre:'Clientes',
     ruta:'/clientes',
     url:'clientes.html',
     ico:'users',
   },
   {
+    enmenu:false,
+    nombre:'Nuevo Cliente',
+    ruta:'/nuevo_clientes',
+    url:'nuevo_clientes.html',
+    ico:'users',
+  },
+  {
+    enmenu:true,
     nombre:'Ingreso',
     ruta:'/ingreso',
     url:'ingreso.html',
     ico:'plus',
   },
   {
+    enmenu:true,
     nombre:'Egreso',
     ruta:'/egreso',
     url:'egreso.html',
     ico:'minus',
   },
   {
+    enmenu:true,
     nombre:'Movimientos',
     ruta:'/movimientos',
     url:'movimientos.html',
     ico:'list',
   },
   {
+    enmenu:true,
     nombre:'Caja',
     ruta:'/caja',
     url:'caja.html',
     ico:'cart-arrow-down',
+  },
+  {
+    enmenu:false,
+    nombre:'Clientes',
+    ruta:'/clientes/:id',
+    url:'cliente_id.html',
+    ico:'users',
   },
 ]
 
@@ -67,27 +89,39 @@ app.controller('MenuController', function($scope) {
 });
 
 
-app.controller('clientController', function($scope, $http) {
+app.controller('clientController', function($scope,$route, $http) {
         $scope.cliente = {};
+        $scope.cliente.extra = "";
+        $scope.lista = {};
+        $scope.clean = function (){
+          $scope.message = "";
+          $scope.state ="";
+        };
+        $scope.updateList = function (){
+          $http({
+            method: 'GET',
+            url: baseapi+'/cliente'
+          }).then(function successCallback(response) {
+            $scope.lista = response.data;
+          }, function errorCallback(response) {
+            $scope.lista = {};
+          });
+        };
         $scope.submitForm = function() {
         $http({
           method  : 'POST',
           url     : baseapi+'/cliente',
-          data    : $scope.cliente,
-          headers: {
-            'Content-Type': "Content-Type: application/json"
-          }
+          data: $scope.cliente,
+          headers: {'Content-Type': 'application/x-www-form-urlencoded'}
         }).then(function (data){
-          $scope.message = data;
-           if (data.data) {
-             $scope.state = "success";
-             $scope.message = "Cliente cargado correctamente.";
-           } else {
-             $scope.state = "error";
-             $scope.message = "Verifique que los datos sean correctos.";
-           }
-           $scope.message = data;
-         });
-
+          if (data.data) {
+           $scope.state = "success";
+           $scope.message = "Cliente cargado correctamente.";
+          } else {
+           $scope.state = "error";
+           $scope.message = "Verifique que los datos sean correctos.";
+          }
+        });
+        location.reload();
         };
     });
--- ilusiones/views/cliente/post.php
+++ ilusiones/views/cliente/post.php
@@ -1,14 +1,14 @@
 <?php
 require_once 'classes/cliente.php';
-$params = $request->getParams();
-if(!isset($params['nombre']) || !isset($params['telefono']) || !isset($params['extra'])){
+$params = json_decode(file_get_contents('php://input'), true);
+if(!isset($params['nombre']) || !isset($params['telefono']) || !isset($params['extra']) || empty($params['nombre'])|| empty($params['telefono'])){
   $response->write(json_encode(false));
   return $response;
 }
 $cliente = new Cliente();
-$cliente->setNombre($_REQUEST['nombre']);
-$cliente->setTelefono($_REQUEST['telefono']);
-$cliente->setExtra($_REQUEST['extra']);
+$cliente->setNombre($params['nombre']);
+$cliente->setTelefono($params['telefono']);
+$cliente->setExtra($params['extra']);
 $response->write(json_encode($cliente->Save()));
 return $response;
 ?>
